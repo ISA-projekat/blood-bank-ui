c44263618f0f4fee7b3a8c3aa6f127d2d29d89fd
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2022-11-12 10:40:35 +0100

    Add component file and route it to /bloodbank

---
+++ src/components/bloodBankComponent.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+export default class BloodBankComponent extends Component {
+
+    render() {
+        return (
+            <p>Hello</p>
+        );
+    }
+}
