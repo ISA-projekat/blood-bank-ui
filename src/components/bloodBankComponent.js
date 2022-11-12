91658d9f4eff68765974d71e4750f125e0c4bfd8
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2022-11-12 11:44:08 +0100

    Add searching for bloodbanks

--- src/components/bloodBankComponent.js
+++ src/components/bloodBankComponent.js
@@ -6,7 +6,64 @@ export default class BloodBankComponent extends Component {
         super(props);
         
         this.state = {
-            bloodbanks: []
+            bloodbanks: [],
+            name: '',
+            city: ''
+        }
+
+        this.onFormNameChange = this.onFormNameChange.bind(this);
+        this.onFormCityChange = this.onFormCityChange.bind(this);
+
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+    }
+
+    onFormNameChange(event) {
+        this.setState({name: event.target.value});
+    }
+
+    onFormCityChange(event) {
+        this.setState({city: event.target.value});
+    }
+
+    onFormSubmit(event) {
+        if(this.state.name === '' && this.state.city === '') {
+            fetch("http://localhost:8080/bloodbank/search", {
+            method: 'GET'
+            })
+            .then(res => res.json())
+            .then(data => this.setState({bloodbanks: data}));
+            event.preventDefault();
+            return;
+        }
+        
+        if(this.state.city === '') {
+            fetch(`http://localhost:8080/bloodbank/search?name=${this.state.name}`, {
+            method: 'GET'
+            })
+            .then(res => res.json())
+            .then(data => this.setState({bloodbanks: data}));
+            event.preventDefault();
+            return;
+        }
+
+        if(this.state.name === '') {
+            fetch(`http://localhost:8080/bloodbank/search?city=${this.state.city}`, {
+            method: 'GET'
+            })
+            .then(res => res.json())
+            .then(data => this.setState({bloodbanks: data}));
+            event.preventDefault();
+            return;
+        }
+
+        if(!(this.state.name === '') && !(this.state.city === '')) {
+            fetch(`http://localhost:8080/bloodbank/search?name=${this.state.name}&city=${this.state.city}`, {
+            method: 'GET'
+            })
+            .then(res => res.json())
+            .then(data => this.setState({bloodbanks: data}));
+            event.preventDefault();
+            return;
         }
     }
 
@@ -15,17 +72,37 @@ export default class BloodBankComponent extends Component {
             method: 'GET'
         })
         .then(res => res.json())
-        .then(data => this.setState({bloodbanks: data}))
+        .then(data => this.setState({bloodbanks: data}));
     }
 
     render() {
         return (
             <div>
-                <ul>
-                    {this.state.bloodbanks.map((bb) =>
-                        <li key={bb.id}>Name: {bb.name} Rating: {bb.rating}</li>
-                    )}
-                </ul>
+                <div>
+                    <form onSubmit={this.onFormSubmit}>
+                        <label>
+                            Name:
+                            <input type="text" value={this.state.name} onChange={this.onFormNameChange} />
+                        </label>
+
+                        <label>
+                            City:
+                            <input type="text" value={this.state.city} onChange={this.onFormCityChange} />
+                        </label>
+
+                        <input type="submit" value="Search" />
+                    </form>
+                </div>
+
+                <div>
+                    <ul>
+                        {
+                            this.state.bloodbanks.map((bb) =>
+                                <li key={bb.id}>Name: {bb.name} Rating: {bb.rating}</li>
+                            )
+                        }
+                    </ul>
+                </div>
             </div>
         );
     }
